f66a56b70d06621365cb2c877d2b6605eeae5efc
Yavor Markov <qvor.markov@abv.bg>
2025-02-19 09:56:03 +0200

    Cookies save datetime automatically

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using AutomaticSave.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AutomaticSave.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        string currentTime = "";
+        public IActionResult Index()
+        {
+            currentTime = Request.Cookies["Time"] ?? DateTime.Now.ToString();
+
+            Response.Cookies.Append("Time", currentTime, new CookieOptions
+            {
+                Expires = DateTime.UtcNow.AddDays(1),
+                HttpOnly = true,
+                IsEssential = true
+            });
+
+            Response.Cookies.Delete("Time");
+            ViewBag.Time = currentTime;
+            
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+
+    } }
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutomaticSave.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
