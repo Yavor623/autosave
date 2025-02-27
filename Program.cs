f45bf47dcb3751df7cd6fd91eb9812b948f6c3d5
Yavor623 <127932022+Yavor623@users.noreply.github.com>
2025-02-27 08:20:54 +0200

    Update Program.cs
    Changed the language of a comment

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace AutomaticSave
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Конфигурира HTTP заявката.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
