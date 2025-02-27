c30d2e8aecff3afa00423faa1ebc896381845d1d
Yavor623 <127932022+Yavor623@users.noreply.github.com>
2025-02-27 08:11:45 +0200

    Update Program.cs
    Changed the language of a comment

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace AutomaticSave
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Добавяме услуги на контейнера
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
